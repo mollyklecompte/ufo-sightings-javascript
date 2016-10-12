b26306cc56dd2aec421b41bfbbe520a82add4142
Molly LeCompte <mollylc92@hotmail.com>
2016-10-12 16:04:40 -0700

    fix issue in scripts.

--- js/boring_sighting-interface.js
+++ js/boring_sighting-interface.js
@@ -1,34 +1,15 @@
-// var apiKey = require('./../.env').apiKey;
-//
-// function initMap() {
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: -34.397, lng: 150.644},
-//     zoom: 8
-//   });
-// // }
-//
-// $(document).ready(function() {
-//
-//   $.getScript("http://maps.google.com/maps/api/js?sensor=true&&async=2&callback=MapApiLoaded", function () {});
-//
-//   function MapApiLoaded() {
-//     var map = new google.maps.Map(document.getElementById('map'), {
-//       center: {lat: -34.397, lng: 150.644},
-//       zoom: 8
-//     });
-//   }
-//
-//   var myOptions = {
-//     zoom : 8,
-//     center : lat: -34.397, lng: 150.644
-//   };
-//
-//   var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
-//
-//
-// });
+var apiKey = require('./../.env').apiKey;
 
 
-  // src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCrBmMmrT1dtWPTRnd05td4JxZQ6sRc64w&callback=initMap">
 
-// key=AIzaSyCrBmMmrT1dtWPTRnd05td4JxZQ6sRc64w&callback=initMap
+function addScript() {
+  var newScript = document.createElement("script");
+  newScript.src = "https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&callback=initMap";
+
+  var placeholder = document.getElementById("placeholder");
+  document.body.insertBefore(newScript, placeholder);
+}
+
+$(document).ready(function() {
+  addScript();
+});
